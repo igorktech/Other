f1de4e663599347cb7c27b5b552d95c8da101070
Igor <igoranchikkuzmin@yandex.ru>
2022-08-30 15:03:51 +0300

    Automatic notification bot
    little cupid bot for romantic day

--- test.py
+++ test.py
@@ -1,67 +1,35 @@
 import config
-# import logging
-#
-# from aiogram import Bot, Dispatcher, executor, types
-# #web hook or long-polling
-#
-# logging.basicConfig(level=logging.INFO)
-#
-# # bot init
-# bot = Bot(token=config.TOKEN)
-# dp = Dispatcher(bot)
-#
-# #echo
-# @dp.message_handler()
-# async def echo(message: types.Message):
-#     await message.answer(message.text)
-#
-# # run long-polling
-# if __name__ == "__main__":
-#     executor.start_polling(dp,skip_updates=True)
-#
-
-
-import telebot, wikipedia, re
+import telebot
+import time
+import random
 # Create bot
 bot = telebot.TeleBot(token=config.TOKEN)
-# Choose language Wikipedia
-wikipedia.set_lang("ru")
-# Clear text from Wikipedia
-def getwiki(s):
-    try:
-        ny = wikipedia.page(s)
-        # Get first 1000 symbols
-        wikitext=ny.content[:1000]
-        # Split points
-        wikimas=wikitext.split('.')
-        # delete all after last point
-        wikimas = wikimas[:-1]
-        # Result
-        wikitext2 = ''
-        # All strings except title
-        for x in wikimas:
-            if not('==' in x):
-                # if in string there are more than 3 symbols add to result
-                if(len((x.strip()))>3):
-                   wikitext2=wikitext2+x+'.'
-            else:
-                break
-        # Text filtering with regular expressions
-        #wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        #wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        #wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
-        # Return string
-        return wikitext2
-    # Except wikipedia
-    except Exception as e:
-        return 'В энциклопедии нет информации об этом'
+# Address
+CHANNEL_NAME = "@lovelycupidboy_bot"
+global CHAT_ID
+CHAT_ID = 0
+# Load file
+f = open('lovemassage.txt', 'r', encoding='UTF-8')
+massages = f.read().split('\n')
+f.close()
+print(time.strftime("%D", time.localtime()))
 # Command handler /start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
-# get message from user
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    bot.send_message(message.chat.id, getwiki(message.text))
-# start polling
+    bot.send_message(m.chat.id, 'Your personal cupid')
+    while (time.strftime("%d", time.localtime())) == "30":
+        if time.strftime("%H:%M", time.localtime()) =="10:00":
+            bot.send_message(m.chat.id, "It's time to start your trip sweetheart")
+        if time.strftime("%H:%M", time.localtime()) =="11:00":
+            bot.send_message(m.chat.id, "Next station is on a plate")
+        if time.strftime("%H:%M", time.localtime()) =="12:00":
+            bot.send_message(m.chat.id, "May we try to swim?")
+        if time.strftime("%H:%M", time.localtime()) == "13:45":
+            bot.send_message(m.chat.id, "What is the best idea to get powers after the sea trip?")
+        if time.strftime("%H:%M", time.localtime()) == "17:00":
+            bot.send_message(m.chat.id, "I really recommend you relax")
+        if int(time.strftime("%H", time.localtime()))%4 == 0:
+            bot.send_message(m.chat.id, random.choice(massages))
+        time.sleep(40)
+
 bot.polling(none_stop=True, interval=0)
