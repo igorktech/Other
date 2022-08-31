943132b734436d8ebed26a26357d981aa67bf110
Igor <igoranchikkuzmin@yandex.ru>
2022-09-01 01:04:37 +0300

    pre-release

--- test.py
+++ test.py
@@ -6,10 +6,8 @@ import random
 bot = telebot.TeleBot(token=config.TOKEN)
 # Address
 CHANNEL_NAME = "@lovelycupidboy_bot"
-global CHAT_ID
-CHAT_ID = 0
 # Load file
-f = open('Other/lovemassage.txt', 'r', encoding='UTF-8')
+f = open('lovemassage.txt', 'r', encoding='UTF-8')
 massages = f.read().split('\n')
 f.close()
 print(time.strftime("%D", time.localtime()))
@@ -17,19 +15,22 @@ print(time.strftime("%D", time.localtime()))
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Your personal cupid')
-    while (time.strftime("%d", time.localtime())) == "31":
-        if time.strftime("%H:%M", time.localtime()) =="20:00":
+    while ((time.strftime("%d", time.localtime())) == "01") or ((time.strftime("%d", time.localtime())) == "02"):
+        if time.strftime("%H:%M", time.localtime()) =="09:00":
             bot.send_message(m.chat.id, "It's time to start your trip sweetheart")
-        if time.strftime("%H:%M", time.localtime()) =="20:30":
+        if time.strftime("%H:%M", time.localtime()) =="11:00":
             bot.send_message(m.chat.id, "Next station is on a plate")
-        if time.strftime("%H:%M", time.localtime()) =="20:45":
+        if time.strftime("%H:%M", time.localtime()) =="12:40":
             bot.send_message(m.chat.id, "May we try to swim?")
-        if time.strftime("%H:%M", time.localtime()) == "21:45":
+        if time.strftime("%H:%M", time.localtime()) == "13:45":
             bot.send_message(m.chat.id, "What is the best idea to get powers after the sea trip?")
-        if time.strftime("%H:%M", time.localtime()) == "22:00":
+        if time.strftime("%H:%M", time.localtime()) == "17:00":
             bot.send_message(m.chat.id, "I really recommend you relax")
         if (int(time.strftime("%H", time.localtime()))%2 == 0)and(int(time.strftime("%M", time.localtime())) == 0):
             bot.send_message(m.chat.id, random.choice(massages))
-        time.sleep(30)
+        time.sleep(60)
 
+    for massage in massages:
+        bot.send_message(m.chat.id, random.choice(massages))
+        time.sleep(18000)
 bot.polling(none_stop=True, interval=0)
