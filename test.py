4df072d0bc08ff0f40e759c4f1f246052f77d5cc
Igor <igoranchikkuzmin@yandex.ru>
2022-09-01 12:54:54 +0300

    pre-release

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(token=config.TOKEN)
 # Address
 CHANNEL_NAME = "@lovelycupidboy_bot"
 # Load file
-f = open('lovemassage.txt', 'r', encoding='UTF-8')
+f = open('Other/lovemassage.txt', 'r', encoding='UTF-8')
 massages = f.read().split('\n')
 f.close()
 print(time.strftime("%D", time.localtime()))
@@ -15,12 +15,12 @@ print(time.strftime("%D", time.localtime()))
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Your personal cupid')
-    while ((time.strftime("%d", time.localtime())) == "01") or ((time.strftime("%d", time.localtime())) == "02"):
+    while int(time.strftime("%d", time.localtime())) <= 2:
         if time.strftime("%H:%M", time.localtime()) =="09:00":
             bot.send_message(m.chat.id, "It's time to start your trip sweetheart")
         if time.strftime("%H:%M", time.localtime()) =="11:00":
             bot.send_message(m.chat.id, "Next station is on a plate")
-        if time.strftime("%H:%M", time.localtime()) =="12:40":
+        if time.strftime("%H:%M", time.localtime()) =="12:20":
             bot.send_message(m.chat.id, "May we try to swim?")
         if time.strftime("%H:%M", time.localtime()) == "13:45":
             bot.send_message(m.chat.id, "What is the best idea to get powers after the sea trip?")
@@ -30,7 +30,8 @@ def start(m, res=False):
             bot.send_message(m.chat.id, random.choice(massages))
         time.sleep(60)
 
-    for massage in massages:
-        bot.send_message(m.chat.id, random.choice(massages))
-        time.sleep(18000)
+    if int(time.strftime("%d", time.localtime())) > 2:
+        for massage in massages:
+            bot.send_message(m.chat.id, random.choice(massages))
+            time.sleep(18000)
 bot.polling(none_stop=True, interval=0)
