8589b0283ad62b233d728a4fc692fa12390fbcb4
Igor <igoranchikkuzmin@yandex.ru>
2022-09-12 22:26:11 +0300

    Task 3

--- main.cpp
+++ main.cpp
@@ -1,47 +1,89 @@
 #include <iostream>
 #include <string>
-#include <set>
-#include <unordered_map>
+#include <queue>
+#include <vector>
+
+
+void
+getVector(int col, int line, std::vector<std::vector<char>> &matrix, std::queue<std::pair<int, int>> &queue_matrix);
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    std::string A, B;
+    int col = 0, line = 0;
+    std::cin >> col;
+    std::cin >> line;
+    std::vector<std::vector<char>> matrix(col, std::vector<char>(line));
 
-    std::cin >> A;
-    std::cin >> B;
-    std::string cont = B;
-    std::unordered_map<char, int> umap;
-    for (auto ch: A) {
-        auto iter = umap.find(ch);
-        if (iter != umap.end()) {
-            iter->second += 1;
-        } else {
-            umap.insert(std::pair<char, int>(ch, 1));
-        }
 
+    for (int i = 0; i < col; i++) {
+        std::string tmp;
+        std::cin >> tmp;
+        matrix[i] = std::vector<char>(tmp.begin(), tmp.end());
     }
-    for (int i = 0; i < A.length(); i++) {
-        auto iter = umap.find(B[i]);
-        if (A[i] == B[i]) {
-            cont[i] = 'P';
-            umap[B[i]] -= 1;
 
+
+    std::queue<std::pair<int, int>> queue_matrix;
+    getVector(col, line, matrix, queue_matrix);
+
+    while (!queue_matrix.empty()) {
+        std::pair first_pair = queue_matrix.front();
+        queue_matrix.pop();
+
+        if (matrix[first_pair.first][first_pair.second + 1] == '.') {
+            queue_matrix.push(std::pair<int, int>(first_pair.first, first_pair.second + 1));
+            matrix[first_pair.first][first_pair.second + 1] = 'L';
+        }
+        if (matrix[first_pair.first][first_pair.second - 1] == '.') {
+            queue_matrix.push(std::pair<int, int>(first_pair.first, first_pair.second - 1));
+            matrix[first_pair.first][first_pair.second - 1] = 'R';
+        }
+        if (matrix[first_pair.first + 1][first_pair.second] == '.') {
+            queue_matrix.push(std::pair<int, int>(first_pair.first + 1, first_pair.second));
+            matrix[first_pair.first + 1][first_pair.second] = 'U';
+        }
+        if (matrix[first_pair.first - 1][first_pair.second] == '.') {
+            queue_matrix.push(std::pair<int, int>(first_pair.first - 1, first_pair.second));
+            matrix[first_pair.first - 1][first_pair.second] = 'D';
         }
     }
-    for (int i = 0; i < A.length(); i++) {
-        auto iter = umap.find(B[i]);
-        if (B[i] != A[i]) {
 
-            if ((iter != umap.end()) && (iter->second >= 1)) {
-                cont[i] = 'S';
-                umap[B[i]] -= 1;
-            } else {
-                cont[i] = 'I';
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < line; j++) {
+            std::cout << matrix[i][j];
+        }
+        if (i + 1 != col) {
+            std::cout << std::endl;
+        }
+    }
+    return 0;
+}
+
+void
+getVector(int col, int line, std::vector<std::vector<char>> &matrix, std::queue<std::pair<int, int>> &queue_matrix) {
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < line; j++) {
+            if (matrix[i][j] == 'S') {
+                if (matrix[i][j + 1] == '.') {
+                    queue_matrix.push(std::pair<int, int>(i, j + 1));
+                    matrix[i][j + 1] = 'L';
+                }
+                if (matrix[i][j - 1] == '.') {
+                    queue_matrix.push(std::pair<int, int>(i, j - 1));
+                    matrix[i][j - 1] = 'R';
+                }
+                if (matrix[i + 1][j] == '.') {
+                    queue_matrix.push(std::pair<int, int>(i + 1, j));
+                    matrix[i + 1][j] = 'U';
+                }
+                if (matrix[i - 1][j] == '.') {
+                    queue_matrix.push(std::pair<int, int>(i - 1, j));
+                    matrix[i - 1][j] = 'D';
+                }
+                return;
             }
         }
     }
-    std::cout << cont;
-    return 0;
+    return;
 }
